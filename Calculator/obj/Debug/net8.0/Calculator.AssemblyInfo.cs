027937edf905234e51b6a89dd9ef09ab92419e96
Rob-exe <robert.ernest@mensa.cz>
2024-10-25 10:10:49 +0200

    Something or other. Create new project for arrays because booooored.

--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+491a7c18803cf8ad6d89554c86830328767854a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17dd87c426bb881d0b13d4dc1929daa04629c764")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
